b91adeca8ed697d0d4bd107e24b3b240ae342cef
Christopher Dwyer <chris@chrisdwyer.me>
2016-08-27 16:44:42 -0400

    Added Firebase

---
+++ Pods/Target Support Files/Pods-Liftr/Pods-Liftr-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Liftr : NSObject
+@end
+@implementation PodsDummy_Pods_Liftr
+@end
---
+++ Pods/Target Support Files/Pods-Liftr/Pods-Liftr-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_LiftrVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LiftrVersionString[];
+
